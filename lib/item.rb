1e3616a2d0e8b95153ac387ff8b5457409694339
Hashim Gari <hashmaster3k@gmail.com>
2020-08-05 08:58:55 -0600

    Adds readable attributes to item class

--- lib/item.rb
+++ lib/item.rb
@@ -1,5 +1,9 @@
 class Item
-  def initialize(item)
 
+  attr_reader :name, :price
+
+  def initialize(item)
+    @name  = item[:name]
+    @price = item[:price].split("$")[1].to_f.round(2)
   end
 end
--- test/item_test.rb
+++ test/item_test.rb
@@ -7,4 +7,10 @@ class ItemTest <  Minitest::Test
     item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
     assert_instance_of Item, item1
   end
+
+  def test_readable_attributes
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    assert_equal 'Peach Pie (Slice)', item1.name
+    assert_equal 3.75, item1.price
+  end
 end
