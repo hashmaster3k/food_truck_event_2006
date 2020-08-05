2fa12f06a198bfa925d717248699d4211b6efe5b
Hashim Gari <hashmaster3k@gmail.com>
2020-08-05 09:06:06 -0600

    Adds method to add to stock

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -20,4 +20,15 @@ class FoodTruckTest <  Minitest::Test
     item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
     assert_equal 0, food_truck.check_stock(item1)
   end
+
+  def test_add_to_stock
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    food_truck.stock(item1, 30)
+
+    expected = {item1 => 30}
+
+    assert_equal expected, food_truck.inventory
+    assert_equal 30, food_truck.check_stock(item1)
+  end
 end
