1e036a76f2ddeddd7e246743d235b975fab177f6
Hashim Gari <hashmaster3k@gmail.com>
2020-08-05 09:00:40 -0600

    Adds food truck class and test

---
+++ test/food_truck_test.rb
@@ -0,0 +1,10 @@
+require './lib/food_truck'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class FoodTruckTest <  Minitest::Test
+  def test_it_exists
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    assert_instance_of FoodTruck, food_truck
+  end
+end
